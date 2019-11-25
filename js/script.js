cfe85cecacf982f803264cef953ce07c46e201d9
Alexia <bisiaux.alexia@hotmail.fr>
2019-11-25 17:02:35 +0100

    animjs

--- js/script.js
+++ js/script.js
@@ -1,10 +1,43 @@
 $(function(){
+
     let navbar = document.getElementById('navbar-menu');
     document.body.setAttribute("data-offset",navbar.offsetHeight + 2);
+
     $(".navbar a").on("click",function(event){
         event.preventDefault();
         var hash = this.hash;
         $("html,body").animate({scrollTop: $(hash).offset().top - navbar.offsetHeight },900,function(){})
     });
+
+
+var isFinish = false;
+
+    $(window).scroll(function() {
+        var hT = $('#skillDescription').offset().top,
+            hH = $('#skillDescription').outerHeight(),
+            wH = $(window).height(),
+            wS = $(this).scrollTop();
+
+        if ((wS > (hT+hH-wH) && (hT > wS) && (wS+wH > hT+hH)) && !isFinish){
+            anime({
+                targets: '.logo-extra ',
+                translateX: 1500,
+                delay: anime.stagger(300, {easing: 'easeOutQuad'}),
+            });
+            anime({
+                targets: '.logo-back',
+                translateX: 1500,
+                delay: anime.stagger(300, {easing: 'easeOutQuad'}),
+            });
+            anime({
+                targets: '.logo-front',
+                translateX: 1500,
+                delay: anime.stagger(300, {easing: 'easeOutQuad'}),
+            });
+            isFinish = true
+        } else {
+            
+        }
+     });
 
 });
