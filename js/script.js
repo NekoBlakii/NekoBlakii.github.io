2a22ab32a3e12148ba4947de918e62732b207671
Alexia <bisiaux.alexia@hotmail.fr>
2019-11-28 16:59:32 +0100

    MobileNoAnimation

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 $(function(){
     $(window).scrollTop(0);
 
+    if(window.matchMedia("(max-width: 768px)").matches)
+    {
+        let desktopElements = document.querySelectorAll(".desktop");
+        desktopElements.forEach(element => {
+            element.classList.remove("desktop");
+        });
+    }
+
     let navbar = document.getElementById('navbar-menu');
     document.body.setAttribute("data-offset",navbar.offsetHeight + 2);
 
@@ -30,74 +38,108 @@ $(function(){
            return isScrolled;
         };
 
-        if(isScrolled('#skillDescription') && !isFinish){
-            anime({
-                targets: '.logo-front',
-                translateX: 1500,
-                delay: anime.stagger(500),
-                easing: 'easeInOutSine'
-            });
-            anime({
-                targets: '.logo-back',
-                translateX: 1500,
-                delay: anime.stagger(500),
-                easing: 'easeInOutSine'
-            });
-            anime({
-                targets: '.logo-extra',
-                translateX: 1500,
-                delay: anime.stagger(500),
-                easing: 'easeInOutSine'
-            });
-            isFinish = true
-        } else {
-            
-        }
-
-        if(isScrolled('#formations-container') && !isOver)
+        if(window.matchMedia("(max-width: 768px)").matches)
         {
-            anime({
-                targets: '.formation1',
-                translateX: 1500,
-                delay: anime.stagger(2000, {easing: 'easeInOutSine'}),
-            });
-            anime({
-                targets: '.formation2',
-                translateY: 1500,
-                delay: anime.stagger(2000, {easing: 'easeInOutSine'}),
-            });
-            anime({
-                targets: '.formation3',
-                translateX: -2000,
-                delay: anime.stagger(2000, {easing: 'easeInOutSine'}),
-            });
-            isOver = true
-        }
-        else{
+            /*
+            if(isScrolled('#skillDescription') && !isFinish){
+                anime({
+                    targets: '.logo-skill',
+                    translateX: 1500,
+                    delay: anime.stagger(500),
+                    easing: 'easeInOutSine'
+                });
+                isFinish = true
+            } 
+    
+            if(isScrolled('#formations-container') && !isOver)
+            {
+                anime({
+                    targets: '.formation',
+                    translateX: 1500,
+                    delay: anime.stagger(2000, {easing: 'easeInOutSine'}),
+                });
 
+                isOver = true
+            }
+    
+            if(isScrolled('#experiences-container') && !isEnded)
+            {
+                anime({
+                    targets: '.col-left',
+                    translateX: 1500,
+                    delay: anime.stagger(300),
+                    easing: 'easeInOutSine'
+                });
+                anime({
+                    targets: '.col-right',
+                    translateX: -2000,
+                    delay: anime.stagger(300),
+                    easing: 'easeInOutSine'
+                });
+                isEnded = true
+            }*/
         }
-
-        if(isScrolled('#experiences-container') && !isEnded)
+        else
         {
-            console.log('coucou');
-            anime({
-                targets: '.col-left',
-                translateX: 1500,
-                delay: anime.stagger(300),
-                easing: 'easeInOutSine'
-            });
-            anime({
-                targets: '.col-right',
-                translateX: -2000,
-                delay: anime.stagger(300),
-                easing: 'easeInOutSine'
-            });
-            isEnded = true
+            if(isScrolled('#skillDescription') && !isFinish){
+                anime({
+                    targets: '.logo-front',
+                    translateX: 1500,
+                    delay: anime.stagger(500),
+                    easing: 'easeInOutSine'
+                });
+                anime({
+                    targets: '.logo-back',
+                    translateX: 1500,
+                    delay: anime.stagger(500),
+                    easing: 'easeInOutSine'
+                });
+                anime({
+                    targets: '.logo-extra',
+                    translateX: 1500,
+                    delay: anime.stagger(500),
+                    easing: 'easeInOutSine'
+                });
+                isFinish = true
+            } 
+    
+            if(isScrolled('#formations-container') && !isOver)
+            {
+                anime({
+                    targets: '.formation1',
+                    translateX: 1500,
+                    delay: anime.stagger(2000, {easing: 'easeInOutSine'}),
+                });
+                anime({
+                    targets: '.formation2',
+                    translateY: 1500,
+                    delay: anime.stagger(2000, {easing: 'easeInOutSine'}),
+                });
+                anime({
+                    targets: '.formation3',
+                    translateX: -2000,
+                    delay: anime.stagger(2000, {easing: 'easeInOutSine'}),
+                });
+                isOver = true
+            }
+    
+            if(isScrolled('#experiences-container') && !isEnded)
+            {
+                anime({
+                    targets: '.col-left',
+                    translateX: 1500,
+                    delay: anime.stagger(300),
+                    easing: 'easeInOutSine'
+                });
+                anime({
+                    targets: '.col-right',
+                    translateX: -2000,
+                    delay: anime.stagger(300),
+                    easing: 'easeInOutSine'
+                });
+                isEnded = true
+            }
         }
-        else{
-
-        }
-
 
      });
 
